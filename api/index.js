e6ef5ac34f196b5c82da1b81ea986f822fab0b64
Roshini2405 <36roshini@gmail.com>
2024-08-24 23:42:00 +0530

    login Successful

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express=require('express');
 const cors=require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 require('dotenv').config();
 const app = express();
 
 const bcryptSalt= bcrypt.genSaltSync(10);
+const jwtSecret = 'uytdhbvy677hfhnbkj';
 
 app.use(express.json());
 app.use(cors({
@@ -34,5 +36,24 @@ app.post('/register',async (req,res)=>{
         res.status(422).json(e);
     }
 });
+
+app.post('/login',async (req,res)=>{
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if (userDoc){
+        const passOk=bcrypt.compareSync(password,userDoc.password);
+        if(passOk){
+            jwt.sign({email:userDoc.email, id:userDoc._id},jwtSecret,{},(err,token)=>{
+                if(err) throw err;
+                res.cookie('token',token).json('pass ok');
+
+            });
+        }else{
+            res.status(422).json('pass not ok');
+        }
+    }else{
+        res.json('Not found');
+    }
+})
 
 app.listen(4000);
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,13 +1,32 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
+import axios from 'axios';
 
 function LoginPage(){
+    const [email,setEmail] = useState('');
+    const[password,setPassword] = useState('');
+    async function handleloginSubmit(ev){
+        ev.preventDefault();
+        try{
+            await axios.post('/login',{email,password});
+            alert("Login Successful");
+        }catch(e){
+            alert("Login Failed");
+        }
+    }
     return (
         <div className="mt-4 grow flex items-center justify-around">
             <div className="mb-32">
             <h1 className="text-3xl text-center ">Login</h1>
-            <form className="max-w-md mx-auto border">
-                <input type="email" placeholder="your@email.com"/>
-                <input type="password" placeholder="Password"/>
+            <form className="max-w-md mx-auto border" onSubmit={handleloginSubmit}>
+                <input type="email"
+                 placeholder="your@email.com"
+                 value={email}
+                 onChange={ev=>setEmail(ev.target.value)} />
+                <input type="password" 
+                placeholder="Password"
+                value={password}
+                onChange={ev=>setPassword(ev.target.value)} />
                 <button className="login">Login</button>
                 <div className="text-center py-2 text-gray-500">
                     Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register Now</Link>
